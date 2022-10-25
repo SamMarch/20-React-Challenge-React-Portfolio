eaf5c232330aca42395e5d4d964570c0b3bf40ee
Sammy <marchsam1988@gmail.com>
2022-10-25 22:22:48 +1030

    about page style added

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function About() {
   return (
-    <div className="bg-blue-500 p-10">
-      <h1>About</h1>
+    <div className="bg-blue-500 p-10 max-w-lg">
+      <h1 className="text-3xl text-center pb-5">About Me</h1>
+      <p>
+        Full-stack web developer with much experience in management of time,
+        staff, and budgets coming from a strong background in hospitality.
+        Having spent 15 years in all areas of the industry and earning all
+        qualifications necessary to do so, I have learned many transferable
+        skills, from working under pressure in a kitchen and within strict time
+        constraints, to strong people skills and risk management in the front of
+        house. I am also currently completing a qualification in Training and
+        Assessment which would allow me to teach any subject I am qualified in,
+        including all the areas mentioned, along with any new skills I become
+        certified in.
+      </p>
     </div>
   );
 }
