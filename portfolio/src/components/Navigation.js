1f9052e8fa8d70d7db79a187a1e4fc3c5daece7a
Sammy <marchsam1988@gmail.com>
2022-10-25 12:08:32 +1030

    changed styling from bootstrap to tailwind

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -2,30 +2,42 @@ import React from "react";
 
 function Navigation({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
+    <ul className="flex flex-wrap list-none pl-0 mb-0 border border-t-0 border-r-0 border-l-0 border-b-1 border-gray-200">
+      <li className="">
         <a
           href="#about"
           onClick={() => handlePageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          className={
+            currentPage === "About"
+              ? "inline-block py-2 px-4 no-underline active"
+              : "inline-block py-2 px-4 no-underline"
+          }
         >
           About Me
         </a>
       </li>
-      <li className="nav-item">
+      <li className="">
         <a
           href="#Project"
           onClick={() => handlePageChange("Project")}
-          className={currentPage === "Project" ? "nav-link active" : "nav-link"}
+          className={
+            currentPage === "Project"
+              ? "inline-block py-2 px-4 no-underline active"
+              : "inline-block py-2 px-4 no-underline"
+          }
         >
           Projects
         </a>
       </li>
-      <li className="nav-item">
+      <li className="">
         <a
           href="#Contact"
           onClick={() => handlePageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+          className={
+            currentPage === "Contact"
+              ? "inline-block py-2 px-4 no-underline active"
+              : "inline-block py-2 px-4 no-underline"
+          }
         >
           Contact Info
         </a>
