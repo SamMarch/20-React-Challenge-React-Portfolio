ba393c4331a0454d961bd38d8e658223c9e89e08
Sammy <marchsam1988@gmail.com>
2022-10-25 22:21:49 +1030

    header styling modified

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -3,12 +3,14 @@ import Navigation from "./Navigation";
 
 function Header({ currentPage, handlePageChange }) {
   return (
-    <div className="bg-red-500 p-10">
-      <h1>Header</h1>
-      <Navigation
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}
-      />
+    <div>
+      <h1 className="bg-red-500 p-5">Sam March</h1>
+      <div className="bg-sky-400">
+        <Navigation
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 }
