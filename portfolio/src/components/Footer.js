3be9b0ddb68c2826a477947f29e5297693a7ae28
Sammy <marchsam1988@gmail.com>
2022-10-25 22:21:15 +1030

    footer styling fixed

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-lime-500 absolute inset-x-0 bottom-0 p-5">
+    <footer className="bg-lime-500 mt-auto p-5">
       <h1>Footer</h1>
     </footer>
   );
